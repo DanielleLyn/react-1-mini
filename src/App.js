c1c367b31695bef484079901eb3308ec75e22123
Danielle Cucinotta <Danielle.cucinotta@gmail.com>
2018-06-22 14:52:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
- <div></div>
-    );
+  constructor (){
+    super();
+    this.state = {
+      picture: 'cat',
+      name: 'Smiley',
+      friends: [{
+        name: 'My 404 friend',
+        picture: 'https://http.cat/404'
+      },{
+        name: 'My chill cat with the munchies',
+        picture: 'https://http.cat/420'
+      }]
+    }
   }
+
+changeName(name){
+  this.setState({
+    name: name,
+  })
+}
+
+changePicture = (event) => {
+  this.setState({
+    picture: event.target.value,
+  })
+}
+
+render() {
+  return (
+    <div>
+      Name: <input value={this.state.name} onChange={(event) => this.changeName(event.target.value)} />
+      Picture: <input value={this.state.picture} onChange={(event) => this.changePicture(event)} />
+      <div>
+        {this.state.friends.map(friend => {
+          return (
+            <p><img src={friend.picture} alt={friend.name} /> {friend.name}</p>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
 }
 
 export default App;
